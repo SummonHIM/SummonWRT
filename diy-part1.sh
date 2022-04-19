a45738192d9b8a06e8cf95b49bbb90a102314947
SummonHIM <2239760702@qq.com>
2022-04-19 13:42:51 +0800

    update diyp1

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,40 +18,40 @@
 
 # Socks tools
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git openwrt-passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
-echo 'src-git OpenClash https://github.com/vernesong/OpenClash' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main' >>feeds.conf.default
+echo 'src-git OpenClash https://github.com/vernesong/OpenClash.git;master' >>feeds.conf.default
 
 # Tools
-echo 'src-git luci-app-diskman https://github.com/lisaac/luci-app-diskman' >>feeds.conf.default
-echo 'src-git openwrt-iptvhelper https://github.com/riverscn/openwrt-iptvhelper' >>feeds.conf.default
-echo "src-git natelol https://github.com/natelol/natelol" >> feeds.conf.default
-echo "src-git wrtbwmon https://github.com/brvphoenix/wrtbwmon" >> feeds.conf.default
-echo "src-git luci-app-wrtbwmon https://github.com/brvphoenix/luci-app-wrtbwmon" >> feeds.conf.default
-echo "src-git smartdns https://github.com/pymumu/smartdns" >> feeds.conf.default
-echo "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter" >> feeds.conf.default
+echo 'src-git luci-app-diskman https://github.com/lisaac/luci-app-diskman.git;master' >>feeds.conf.default
+echo 'src-git openwrt-iptvhelper https://github.com/riverscn/openwrt-iptvhelper.git;master' >>feeds.conf.default
+echo "src-git natelol https://github.com/natelol/natelol.git" >> feeds.conf.default
+echo "src-git wrtbwmon https://github.com/brvphoenix/wrtbwmon.git;master" >> feeds.conf.default
+echo "src-git luci-app-wrtbwmon https://github.com/brvphoenix/luci-app-wrtbwmon.git;master" >> feeds.conf.default
+echo "src-git smartdns https://github.com/pymumu/smartdns.git;master" >> feeds.conf.default
+echo "src-git OpenAppFilter https://github.com/destan19/OpenAppFilter.git;master" >> feeds.conf.default
 echo 'src-git linkease https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git linkease_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 echo 'src-git linkease_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 
-echo 'src-git MentoHUST-OpenWrt-ipk https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk' >>feeds.conf.default
-echo 'src-git luci-app-mentohust https://github.com/BoringCat/luci-app-mentohust' >>feeds.conf.default
-echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome' >>feeds.conf.default
-echo "src-git luci-app-serverchan https://github.com/tty228/luci-app-serverchan" >> feeds.conf.default
-echo "src-git ddns-scripts_aliyun https://github.com/sensec/ddns-scripts_aliyun" >> feeds.conf.default
-echo "src-git luci-app-vlmcsd https://github.com/flytosky-f/luci-app-vlmcsd" >> feeds.conf.default
-echo "src-git ddns-scripts_dnspod https://github.com/coolsnowwolf/lede/tree/master/package/lean/ddns-scripts_dnspod" >> feeds.conf.default
-#git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
-#git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
-#git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-#git clone https://github.com/sensec/ddns-scripts_aliyun.git package/ddns-scripts_aliyun
-#git clone https://github.com/flytosky-f/luci-app-vlmcsd.git package/luci-app-vlmcsd
-#wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/ddns-scripts_dnspod/Makefile package/ddns-scripts_dnspod/Makefile
-#wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/ddns-scripts_dnspod/update_dnspod_cn.sh package/ddns-scripts_dnspod/update_dnspod_cn.sh
-#wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/ddns-scripts_dnspod/update_dnspod_com.sh package/ddns-scripts_dnspod/update_dnspod_com.sh
+#echo 'src-git MentoHUST-OpenWrt-ipk https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git;master' >>feeds.conf.default
+#echo 'src-git luci-app-mentohust https://github.com/BoringCat/luci-app-mentohust.git;master' >>feeds.conf.default
+#echo 'src-git luci-app-adguardhome https://github.com/rufengsuixing/luci-app-adguardhome.git;master' >>feeds.conf.default
+#echo "src-git luci-app-serverchan https://github.com/tty228/luci-app-serverchan" >> feeds.conf.default
+#echo "src-git ddns-scripts_aliyun https://github.com/sensec/ddns-scripts_aliyun" >> feeds.conf.default
+#echo "src-git luci-app-vlmcsd https://github.com/flytosky-f/luci-app-vlmcsd" >> feeds.conf.default
+#echo "src-git ddns-scripts_dnspod https://github.com/coolsnowwolf/lede/tree/master/package/lean/ddns-scripts_dnspod" >> feeds.conf.default
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/mentohust
+git clone https://github.com/BoringCat/luci-app-mentohust.git package/luci-app-mentohust
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/sensec/ddns-scripts_aliyun.git package/ddns-scripts_aliyun
+git clone https://github.com/flytosky-f/luci-app-vlmcsd.git package/luci-app-vlmcsd
+wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/ddns-scripts_dnspod/Makefile package/ddns-scripts_dnspod/Makefile
+wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/ddns-scripts_dnspod/update_dnspod_cn.sh package/ddns-scripts_dnspod/update_dnspod_cn.sh
+wget https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/ddns-scripts_dnspod/update_dnspod_com.sh package/ddns-scripts_dnspod/update_dnspod_com.sh
 
 # Themes
-echo 'src-git luci-theme-argon https://github.com/jerrykuku/luci-theme-argon' >> feeds.conf.default
-echo 'src-git luci-app-netdata https://github.com/sirpdboy/luci-app-netdata' >> feeds.conf.default
-#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-#git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-theme-argon-config
+#echo 'src-git luci-theme-argon https://github.com/jerrykuku/luci-theme-argon' >> feeds.conf.default
+#echo 'src-git luci-app-netdata https://github.com/sirpdboy/luci-app-netdata' >> feeds.conf.default
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-theme-argon-config
