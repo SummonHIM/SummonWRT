98f361fc756d51f2bbe91dd7ff538a552356fc38
SummonHIM <2239760702@qq.com>
2022-05-01 12:27:37 +0800

    change local build

--- local-build-x86-64.sh
+++ local-build-x86-64.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+# Useage: sh -c "$(curl -fsSL https://raw.githubusercontent.com/SummonHIM/SummonWRT/master/local-build-x86-64.sh)"
 echo --- Setup Env ---
 sudo apt-get update
 sudo apt-get install build-essential ccache ecj fastjar file g++ gawk gettext git java-propose-classpath libelf-dev libncurses5-dev libncursesw5-dev libssl-dev mkisofs python python2.7-dev python3 unzip wget python-distutils-extra python3-setuptools python3-dev rsync subversion swig time xsltproc zlib1g-dev -y
@@ -10,7 +12,6 @@ df -hT $PWD
 git clone https://git.openwrt.org/openwrt/openwrt.git -b v21.02.3 openwrt
 if [ -d "openwrt" ]; then
     cd openwrt
-    wget https://raw.githubusercontent.com/SummonHIM/SummonWRT/master/x86_64.config -O .config
 else
     exit
 fi
@@ -27,6 +28,7 @@ echo --- Update and install feeds ---
 echo 
 echo --- Modify custom feeds ---
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/SummonHIM/SummonWRT/master/diy-part2.sh)"
+wget https://raw.githubusercontent.com/SummonHIM/SummonWRT/master/x86_64.config -O .config
 
 echo 
 echo --- Download package ---
@@ -35,4 +37,5 @@ echo make defconfig download -j $(($(nproc)+1))
 echo 
 echo --- Compile the firmware ---
 echo "$(($(nproc)+1)) thread compile"
-echo make -j $(($(nproc)+1)) V=s world
+read -s -n1 -p "Press any key to continue or Ctrl + C to quit ... "
+make -j $(($(nproc)+1)) V=s world
